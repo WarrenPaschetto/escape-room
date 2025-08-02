e3af5fced4e040173f3b13f05f37ecba95060936
Warren Paschetto <wdpaschetto@gmail.com>
2025-08-02 09:42:00 -0400

    Update Modal.tsx

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,7 +7,7 @@ export default function Modal() {
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
-            <div className="bg-white p-6 rounded shadlow-lg max-w-sm">
+            <div className="bg-white p-6 rounded shadow-lg max-w-sm">
                 <p>{modal}</p>
                 <button
                     onClick={closeModal}
